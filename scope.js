394ec298f6fef9e27c28e8aea34775d055caa9da
tommy martinez <thomasjohnmartinez@gmail.com>
2026-03-12 11:22:49 -0400

    added sound and dom manipulation

---
+++ scope.js
@@ -0,0 +1,18 @@
+let myVariable = 'kittyKat'
+
+
+
+function funkyFunction() {
+    //console.log(myVariable);
+    otherVariable = 'doggyDog'
+    otherFunkyFunction(otherVariable)
+    //console.log(otherVariable)
+}
+
+function otherFunkyFunction(package) {
+    console.log(package)
+}
+
+funkyFunction()
+
+
---
+++ sketch.js
@@ -0,0 +1,33 @@
+let sound, amp, cnv;
+let imgSelecter = document.querySelector('#imgStuff');
+
+function preload() {
+  //replace this sound with something we can distribute
+  sound = loadSound('sounds/TragosAmargos.mp3');
+}
+
+function setup() {
+  cnv = createCanvas(400, 400);
+  cnv.parent('p5-stuff')
+  cnv.mousePressed(playSound);
+  textAlign(CENTER);
+  fill(255);
+  amp = new p5.Amplitude();
+  sound.connect(amp);
+  //select DOM element using p5's select function
+  //let imgSelector = Document.querySelector('#imgStuff');
+  describe('The color of the background changes based on the amplitude of the sound.');
+}
+ 
+function playSound() {
+  sound.play();
+}
+ 
+function draw() {
+  let level = amp.getLevel();
+  //maps one range of values to another range of values
+  level = map(level, 0, 0.2, 0, 255);
+  //background(level, 0, 0);
+  imgSelecter.style.width = level + '%';
+  text('click to play', width/2, height/2);
+}
